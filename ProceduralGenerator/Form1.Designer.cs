af38532673324b341c4a44320072171e3df32aa5
Dmitry <dmitriy.stopkevich@gmail.com>
2021-10-24 18:34:28 +0400

    Add project files.

---
+++ ProceduralGenerator/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace ProceduralGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(17, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 787);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1018, 811);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Uni";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProceduralGenerator/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProceduralGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Task.Run(RenderProcedural);
+
+            //RenderProcedural();
+        }
+
+        void RenderProcedural()
+        {
+            DateTime dateTimeFrom = DateTime.Now;
+
+            while (true)
+            {
+                var dateTimeTo = DateTime.Now;
+                double elapsedTime = (dateTimeTo - dateTimeFrom).TotalSeconds;
+                dateTimeFrom = dateTimeTo;
+                
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            using var brushB = new SolidBrush(Color.Black);
+            using var brushW = new SolidBrush(Color.White);
+
+            //gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.Clear(Color.Black);
+
+            for (int h = 0; h < pictureBox1.Height; h++)
+            {
+                for (int w = 0; w < pictureBox1.Width; w++)
+                {
+                    int seed = h | w;
+
+                    Random random = new Random(seed);
+
+                    int rand = random.Next(0, 20);
+
+                    g.FillRectangle(rand == 0 ? brushW : brushB, w, h, 1, 1);
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                pictureBox1.Invalidate();
+            }
+        }
+
+        //{
+        //    var dateTimeTo = DateTime.Now;
+        //    double elapsedTime = (dateTimeTo - dateTimeFrom).TotalSeconds;
+        //    dateTimeFrom = dateTimeTo;
+
+        //    using var bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        //    using var gfx = Graphics.FromImage(bmp);
+        //    using var pen = new Pen(Color.White);
+        //    using var brushB = new SolidBrush(Color.Black);
+        //    using var brushW = new SolidBrush(Color.White);
+
+        //    //gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        //    gfx.Clear(Color.Black);
+
+        //    for (int h = 0; h<bmp.Height; h++)
+        //    {
+        //        for (int w = 0; w<bmp.Width; w++)
+        //        {
+        //            int seed = h | w;
+
+        //            Random = new Random(seed);
+
+        //            int rand = Random.Next(0, 20);
+
+        //            gfx.FillRectangle(rand == 0 ? brushW : brushB, w, h, 1, 1);
+        //        }
+        //    }
+
+        //    pictureBox1.Image?.Dispose();
+        //    pictureBox1.Image = (Bitmap)bmp.Clone();
+        //}
+
+        //void RenderLines()
+        //{
+        //    using var bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        //    using var gfx = Graphics.FromImage(bmp);
+        //    using var pen = new Pen(Color.White);
+
+        //    gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        //    gfx.Clear(Color.Navy);
+        //    for (int i = 0; i < 1000; i++)
+        //    {
+        //        var pt1 = new Point(Random.Next(bmp.Width), Random.Next(bmp.Height));
+        //        var pt2 = new Point(Random.Next(bmp.Width), Random.Next(bmp.Height));
+        //        gfx.DrawLine(pen, pt1, pt2);
+        //    }
+
+
+        //    pictureBox1.Image?.Dispose();
+        //    pictureBox1.Image = (Bitmap)bmp.Clone();
+        //}
+
+
+    }
+}
